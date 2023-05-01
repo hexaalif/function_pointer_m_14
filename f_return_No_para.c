56b0805f0b217f0862924276bda41743d1471ced
hexaalif <hexaalif2020@gmail.com>
2023-05-01 13:13:51 +0600

    return and no parameter

--- f_return_No_para.c
+++ f_return_No_para.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
 
-int sum(int x, int y)
+// int sum(int x, int y)
+// {
+//     int sum = x + y;
+//     return sum;
+// }
+int sum()
 {
-    int sum = x + y;
-    return sum;
+    int x, y;
+    scanf("%d %d", &x, &y);
+    int s = x + y;
+    return s;
 }
 
 int main()
 {
-    int x, y;
-    scanf("%d %d", &x, &y);
-    printf("%d", sum(x, y));
+
+    printf("%d", sum());
     return 0;
 }

b2883198a53ed0ae8fe113e1488cfaea05ef2094
hexaalif <hexaalif2020@gmail.com>
2023-05-01 15:52:24 +0600

    math operations

--- math.c
+++ math.c
@@ -3,14 +3,18 @@
 
 int main()
 {
-    double x, y;
-    scanf("%lf %lf", &x, &y);
+    // double x, y;
+    // scanf("%lf %lf", &x, &y);
     // int ans = ceil(x);
     // int ans = floor(x);
     // int ans = round(x);
     // int ans = sqrt(x);
-    double ans = pow(x * 1.0, y * 1.0);
-    // int ans = round(x);
-    printf("%0.2lf", ans);
+    // double ans = pow(x * 1.0, y * 1.0);
+
+    int x1;
+    scanf("%d", &x1);
+    int ans = abs(x1);
+    printf("%d", ans);
+
     return 0;
 }

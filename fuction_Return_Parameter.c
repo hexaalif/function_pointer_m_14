8edb20308aeaa0ab69570a553b812a0cd6af8c7a
hexaalif <hexaalif2020@gmail.com>
2023-05-01 12:45:39 +0600

    works

--- fuction_Return_Parameter.c
+++ fuction_Return_Parameter.c
@@ -4,16 +4,23 @@
 //     code ;
 //     return what;
 // }
-int sum(int x, int y)
-{
-    // code here
-    int sum = x + y;
-    return sum;
-}
+// int sum(int x, int y)
+// {
+//     // code here
+//     int sum = x + y;
+//     return sum;
+// }
 
 int main()
 {
     int s = sum(10, 20);
     printf("%d", s);
     return 0;
+}
+
+int sum(int x, int y)
+{
+    // code here
+    int sum = x + y;
+    return sum;
 }

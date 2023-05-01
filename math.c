4815e5ec4a1c15cf0062073a1bd7d47071377b5a
hexaalif <hexaalif2020@gmail.com>
2023-05-01 15:43:09 +0600

    math operations

--- math.c
+++ math.c
@@ -6,7 +6,8 @@ int main()
     double x;
     scanf("%lf", &x);
     // int ans = ceil(x);
-    int ans = floor(x);
+    // int ans = floor(x);
+    int ans = round(x);
     printf("%d", ans);
     return 0;
 }
